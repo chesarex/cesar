5fbbf2d1ee7396545526ebc0a7072cb6ae75360f
chesarex <hcacesar@gmail.com>
2014-11-02 18:55:48 -0200

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -1,10 +1,1 @@
-import pyfits
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-#pra ver o header
-hdulist = pyfits.open('2.fits')
 
-#para ver o cabecalho e a dimensao o aquivos filho 
-hdulist.info()
-print hdulist[0].header
